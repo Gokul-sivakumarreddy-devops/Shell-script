8eb2b0218f67737d0e83a5e6c9ccd7b1018eeb8d
gokulvarma <gokul.penmetsa@gmail.com>
2023-12-13 20:09:07 +0530

    Adding Functions

---
+++ 16-Functions1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE() 
+{
+
+if [ $2 -ne 0 ]     #$? --> if success, it has 0, if failed not zero
+   then
+     echo "ERROR: Git Installation failed"
+     exit 1
+   else
+      echo "Git Installation is successful"
+fi      
+}
+
+
+if [ $ID -ne 0 ]
+  then 
+    echo "ERROR: user do not have root access"
+    exit 1
+  else
+     echo "user has root access"
+
+fi    # fi means reverse of if, indicating condition end
+
+sudo yum install mysql -y   
+VALIDATE $? "Installing MySQL"
+sudo yum install git -y
+VALIDATE $? "Installing Git"
+
