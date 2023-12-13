95e31319b2f08ad1d484b9eed31dc93c3deee8ca
gokulvarma <gokul.penmetsa@gmail.com>
2023-12-13 14:49:53 +0530

    added sixth script

--- 08-variables-with-arguments2.sh
+++ 08-variables-with-arguments2.sh
@@ -2,6 +2,14 @@
 
 echo "Please enter your username"
 
-read USERNAME
+read -s USERNAME        # The value entered above will be automatically atteched to USERNAME variable
+                        # -s --> do not echo input coming from a terminal
 
-echo "User name is : $USERNAME"
+
+echo "Please enter your password"
+read -s PASSWORD
+echo "User password is : $PASSWORD"
+
+echo "User name is : $USERNAME, Password is : $PASSWORD" # I am priting just for validation, you should not print username  and passwords in scripts
+
+
