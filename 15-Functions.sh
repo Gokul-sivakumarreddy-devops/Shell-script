b7be32326fa9d1eba92d1b14a5138b11f633a303
gokulvarma <gokul.penmetsa@gmail.com>
2023-12-13 19:22:35 +0530

    Adding Functions

--- 15-Functions.sh
+++ 15-Functions.sh
@@ -4,14 +4,16 @@ ID=$(id -u)
 
 VALIDATE() 
 {
+
 if [ $? -ne 0 ]     #$? --> if success, it has 0, if failed not zero
    then
      echo "ERROR: Installation failed"
      exit 1
    else
       echo "Installation is successful"
+fi      
 }
-fi
+
 
 if [ $ID -ne 0 ]
   then 
