91a66321764394e47501a96a8eecdaef9530bc84
gokulvarma <gokul.penmetsa@gmail.com>
2023-12-13 18:42:53 +0530

    MySQL

---
+++ 14-Install-MySQL1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+  then 
+    echo "ERROR: user do not have root access"
+    exit 1
+   else
+     echo "user has root access"
+
+     fi    # fi means reverse of if, indicating condition end
+
+sudo yum install mysql -y
+
+if [ $? -ne 0 ]     #$? --> if success, it has 0, if failed not zero
+   then
+     echo "ERROR: MySQL installation failed"
+     exit 1
+   else
+      echo "MySQL installation successfully installed"
+
+
+fi  
+      
