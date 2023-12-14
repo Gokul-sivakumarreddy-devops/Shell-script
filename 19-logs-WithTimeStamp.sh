24ee9329514cb8964229223aaafb9ccfc909a4f6
gokulvarma <gokul.penmetsa@gmail.com>
2023-12-14 11:05:00 +0530

    added timestamp and script name

---
+++ 19-logs-WithTimeStamp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+
+VALIDATE() 
+{
+
+if [ $1 -ne 0 ]     #$? --> if success, it has 0, if failed not zero
+   then
+     echo "ERROR: $2... Installation failed"
+     exit 1
+   else
+      echo "$2...Installation is successful"
+fi      
+}
+
+
+if [ $ID -ne 0 ]
+  then 
+    echo "ERROR: user do not have root access"
+    exit 1
+  else
+     echo "user has root access"
+
+fi    # fi means reverse of if, indicating condition end
+
+sudo yum install mysql -y &>> $LOGFILE
+VALIDATE $? "Installing MySQL"
+sudo yum install git -y
+VALIDATE $? "Installing Git" &>> $LOGFILE
+
