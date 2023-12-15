1d9dd7ef0bad1761223c3385a09747feb8c6af26
gokulvarma <gokul.penmetsa@gmail.com>
2023-12-15 09:51:16 +0530

    logs with colour representation

--- 20-logs-ChangingColours.sh
+++ 20-logs-ChangingColours.sh
@@ -16,19 +16,19 @@ VALIDATE()
 
 if [ $1 -ne 0 ]     #$? --> if success, it has 0, if failed not zero
    then
-     echo "ERROR :  $2... $R Installation failed $N"
+     echo -e "ERROR :  $2... $R Installation failed $N"
      exit 1
    else
-      echo "$2... $G Installation is successful $N"
+      echo -e "$2... $G Installation is successful $N"
 fi      
 }
 
 if [ $ID -ne 0 ]
   then 
-    echo "$R ERROR: user do not have root access $N"
+    echo -e "$R ERROR: user do not have root access $N"
     exit 1
   else
-     echo "$G you are a root user $N"
+     echo -e "$G you are a root user $N"
 
 fi    # fi means reverse of if, indicating condition end
 
