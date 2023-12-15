606c79b4cbc06b4341e8ea374131d4a5f8a3d8c7
gokulvarma <gokul.penmetsa@gmail.com>
2023-12-15 08:33:54 +0530

    logs with colour representation

--- 20-logs-ChangingColours.sh
+++ 20-logs-ChangingColours.sh
@@ -7,24 +7,19 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
-
-
-
 
 VALIDATE() 
 {
 
 if [ $1 -ne 0 ]     #$? --> if success, it has 0, if failed not zero
    then
-     echo "ERROR : $2...$R Installation failed $N"
+     echo "ERROR :  $2... $R Installation failed $N"
      exit 1
    else
-      echo "$2...$G Installation is successful $N"
+      echo "$2... $G Installation is successful $N"
 fi      
 }
-
 
 if [ $ID -ne 0 ]
   then 
