6d5de3296ff49356f52529f5f0da88fa59380b69
gokulvarma <gokul.penmetsa@gmail.com>
2023-12-15 08:24:11 +0530

    logs with colour representation

---
+++ 20-logs-changing colours.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+
+VALIDATE() 
+{
+
+if [ $1 -ne 0 ]     #$? --> if success, it has 0, if failed not zero
+   then
+     echo "ERROR : $2...$R Installation failed $N"
+     exit 1
+   else
+      echo "$2...$G Installation is successful $N"
+fi      
+}
+
+
+if [ $ID -ne 0 ]
+  then 
+    echo "$R ERROR: user do not have root access $N"
+    exit 1
+  else
+     echo "$G you are a root user $N"
+
+fi    # fi means reverse of if, indicating condition end
+
+sudo yum install mysql -y &>> $LOGFILE
+VALIDATE $? "Installing MySQL"
+sudo yum install git -y &>> $LOGFILE
+VALIDATE $? "Installing Git" 
+
