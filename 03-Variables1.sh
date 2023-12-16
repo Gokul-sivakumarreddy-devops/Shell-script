ccea4e7c37388753e633889107d6f0136026fc1c
gokulvarma <gokul.penmetsa@gmail.com>
2023-12-16 09:14:44 +0530

    package Installation

--- 03-Variables1.sh
+++ 03-Variables1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # If we want to change names, we can modify at PERSON1=Ramesh and PERSON2=Suresh
+# Change in single location will effect everywhere automatically.
 
 PERSON1=Ram
 PERSON2=Sam
