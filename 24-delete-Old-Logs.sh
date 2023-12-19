3d2b621b1f23e3b8abba29a2ff742d5014e2a499
gokulvarma <gokul.penmetsa@gmail.com>
2023-12-19 10:24:47 +0530

    delete old logs

---
+++ 24-delete-Old-Logs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SOURCE_DIR=/tmp/shellscript-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d $SOURCE_DIR ]   # denotes not (not a source directory)
+then
+  echo -e "$R Source Directory: $SOURCE_DIR does not exists. $$N"
+  fi
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+
+while IFS= read -r line   # IFS = Internal Field Seperator
+do
+  echo "Deleting the file : $line"
+  rm -rf $line
+done >>> $FILES_TO_DELETE
+
+
+
