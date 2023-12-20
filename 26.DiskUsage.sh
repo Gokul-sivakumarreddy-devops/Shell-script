d5189b25f88a7d62c60da3bfdbc08aca9f2a59e9
gokulvarma <gokul.penmetsa@gmail.com>
2023-12-20 09:33:03 +0530

    Disk Usage

--- 26.DiskUsage.sh
+++ 26.DiskUsage.sh
@@ -11,7 +11,7 @@ do
   
   if [ $usage -ge $DISK_THRESHOLD ]
   then 
-    message+="\n High Disk Usage on $partition: $usage\n"
+    message+= \n "\n High Disk Usage on $partition: $usage\n"
   fi
 done <<< $DISK_USAGE
 
